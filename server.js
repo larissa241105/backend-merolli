f14cdd3ae0601a9db6802799a5b8e02002adefbf
larissa241105 <larissa241105@gmail.com>
2025-08-20 11:47:27 -0400

    consertando as conexoes do bd

--- server.js
+++ server.js
@@ -21,10 +21,11 @@
     // ||                  CONFIGURAÇÃO DO BANCO DE DADOS             ||
     // =================================================================
     const db = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'admin', 
-        database: 'msoft_web'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        port: process.env.DB_PORT
     });
 
     db.connect(err => {
